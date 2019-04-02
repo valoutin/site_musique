cbfbb834935e88ab8213c9fe25b413d22b3b50f8
valentin <laidet.val@hotmail.fr>
2019-04-02 03:43:48 +0200

    la cata

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -38,4 +38,34 @@ class UserType extends AbstractType
             'data_class' => User::class,
         ]);
     }
+
+    // public function buildForm(FormBuilderInterface $builder, array $options)
+    // {
+    //     $builder
+    //         ->add('nom', TextType::class)
+    //         ->add('prenom', TextType::class)
+    //         ->add('email', RepeatedType::class, [
+    //               'type' => EmailType::class,
+    //               'invalid_message' => 'Les deux e-mails doivent t\'être identique.',
+    //               'options' => ['attr' => ['class' => 'password-field']],
+    //               'required' => true,
+    //               'first_options'  => ['label' => 'E-mail:'],
+    //               'second_options' => ['label' => 'Répété votre e-mail:'],
+    //           ])
+    //         ->add('password', RepeatedType::class, [
+    //               'type' => PasswordType::class,
+    //               'invalid_message' => 'Les deux mots de passe doivent t\'être identique.',
+    //               'options' => ['attr' => ['class' => 'password-field']],
+    //               'required' => true,
+    //               'first_options'  => ['label' => 'Mot de passe:'],
+    //               'second_options' => ['label' => 'Répété votre mot de passe:'],
+    //           ]);
+    // }
+    //
+    // public function configureOptions(OptionsResolver $resolver)
+    // {
+    //     $resolver->setDefaults([
+    //         'data_class' => User::class,
+    //     ]);
+    // }
 }
