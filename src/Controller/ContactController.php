635e224f5e0f95eea262884979cc82bca3b54ca1
valentin <laidet.val@hotmail.fr>
2019-04-03 15:00:35 +0200

    securite admin user

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/contact")
+ * @Route("admin/contact")
  */
 class ContactController extends AbstractController
 {
