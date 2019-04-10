77c0189e4260d8d2d9453db865471290155192ab
valentin <laidet.val@hotmail.fr>
2019-04-10 05:21:26 +0200

    bug assert fixed + css pagination

--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -6,15 +6,17 @@ use App\Entity\Event;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 
 class EventType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('titre')
-            ->add('start')
-            ->add('end')
+            ->add('titre', TextType::class)
+            ->add('start', DateTimeType::class)
+            ->add('end', DateTimeType::class)
         ;
     }
 
